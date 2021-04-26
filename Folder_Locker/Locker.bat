ef017f31c06f5e924ced74c71d2c867fc92eece8
Hell3oy <66299492+Hell3oy@users.noreply.github.com>
2021-04-26 21:00:06 +0530

    Add files via upload

---
+++ Folder_Locker/Locker.bat
@@ -0,0 +1,42 @@
+
+cls
+@ECHO OFF
+title Folder Locker
+if EXIST "HTG Locker" goto UNLOCK
+if NOT EXIST Locker goto MDLOCKER
+
+:CONFIRM
+echo Are you sure you want to lock the folder(Y/N)
+set/p "cho=>"
+if %cho%==Y goto LOCK
+if %cho%==y goto LOCK
+if %cho%==n goto END
+if %cho%==N goto END
+echo Invalid choice.
+goto CONFIRM
+
+:LOCK
+ren Locker "HTG Locker"
+attrib +h +s "HTG Locker"
+echo Folder locked
+goto End
+
+:UNLOCK
+echo Enter password to unlock folder
+set/p "pass=>"
+if NOT %pass%== EnterUrPasswordHere goto FAIL
+attrib -h -s "HTG Locker"
+ren "HTG Locker" Locker
+echo Folder Unlocked successfully
+goto End
+
+:FAIL
+echo Invalid password
+goto end
+
+:MDLOCKER
+md Locker
+echo Locker created successfully
+goto End
+
+:End
